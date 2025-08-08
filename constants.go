187442cb170afb63f6ef2055fc03eed836c071f5
deadprogram <ron@hybridgroup.com>
2025-08-08 19:39:17 +0200

    chore: additional constant values for Sphero control
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- constants.go
+++ constants.go
@@ -9,18 +9,28 @@ const (
 	FlagRequestsOnlyErrorResponse = 0x04
 	FlagResetsInactivityTimeout   = 0x08
 
-	DevicePowerInfo    = 0x13
-	DeviceUserIO       = 0x1a
-	DeviceDriving      = 0x16
 	DeviceAPIProcessor = 0x10
 	DeviceSystemInfo   = 0x11
+	DevicePowerInfo    = 0x13
+	DeviceDriving      = 0x16
 	DeviceAnimatronics = 0x17
 	DeviceSensor       = 0x18
+	DeviceUserIO       = 0x1a
+
+	SystemInfoApplicationVersion        = 0x00
+	SystemInfoCommandsBootLoaderVersion = 0x01
 
 	PowerCommandsDeepSleep      = 0x00
 	PowerCommandsSleep          = 0x01
 	PowerCommandsBatteryVoltage = 0x03
 	PowerCommandsWake           = 0x0D
+
+	DrivingCommandsRawMotor      = 0x01
+	DrivingCommandsDriveAsRC     = 0x02
+	DrivingCommandsDriveAsSphero = 0x04
+	DrivingCommandsResetHeading  = 0x06
+	DrivingCommandsWithHeading   = 0x07
+	DrivingCommandsStabilization = 0x0C
 
 	SensorCommandMask                   = 0x00
 	SensorCommandResponse               = 0x02
@@ -28,20 +38,14 @@ const (
 	SensorCommandCollisionDetectedAsync = 0x12
 	SensorCommandResetLocator           = 0x13
 	SensorCommandEnableCollisionAsync   = 0x14
-	SensorCommandSensor1                = 0x0f
 	SensorCommandSensor2                = 0x17
 	SensorCommandConfigureSensorStream  = 0x0c
+	SensorCommandSensor1                = 0x0f
 
 	UserIOCommandsAllLEDs = 0x0e
-
-	DrivingCommandsWithHeading = 0x07
-
-	SystemInfoCommandsBootLoaderVersion = 0x01
 )
 
-// CollisionConfig provides configuration for the collision detection alogorithm.
-// For more information refer to the api specification of "Orbotix Communication API"
-// see also: http://wiki.mark-toma.com/view/Sphero_API_Tutorial
+// CollisionConfig provides configuration for the collision detection algorithm.
 type CollisionConfig struct {
 	// Detection method type to use. Methods 01h and 02h are supported as
 	// of FW ver 1.42. Use 00h to completely disable this service.
